58b05b6335c1d9a506422cf131e243fa085729f7
Kanika <kanika.19jics100@jietjodhpur.ac.in>
2022-08-07 00:45:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 
-checkNumberButton.addEventListener('click',function getValues() {
-        console.log(dateOfBirth.value , luckyNumber.value);
-    })
+function checkBirthDateIsLucky(){
+    const date = dateOfBirth.ariaValueMax;
+    const sum = calculateSum(date);
+}
+function calculateSum(date){
+    
+}
+
+checkNumberButton.addEventListener('click',checkNumberIsLucky)
